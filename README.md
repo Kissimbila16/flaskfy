e6695671c19b861ae6516796ecc8500a59b070ec
Álvaro Mbeia Daniel Miguel <alvarombeiadanielmiguel@gmail.com>
2024-04-07 09:56:22 +0100

    sending project on github

---
+++ README.md
@@ -0,0 +1,14 @@
+This is a lib to help generate flask apps with custom components
+
+
+# Instalation 
+
+```bash
+pip install flaskfy
+```
+
+# Creating new flask project
+
+```bash
+createflaskapp
+```
--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 from setuptools import setup, find_packages
 
-
 setup(
-    name="flaskapp",
+    name="flaskfy",
+    author="Álvaro Mbeia Daniel Miguel",
+    author_email="alvarombeiadanielmiguel@gmail.com",
+    description="This is a lib to help generate flask apps with custom components",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     version="0.0.1",
     packages=find_packages(),
     install_requires=[
+        "python-dotenv",
+        "pymysql",
+        "flask",
     ],
     entry_points = {
         "console_scripts": [
-            "flaskapp = flaskapp"
+            "createflaskapp = flaskfy:criar_projeto_flask"
         ]
     }
 )
