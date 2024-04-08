30b0238bfca00c6d2abc57a04955681e9d9ca0dd
Álvaro Mbeia Daniel Miguel <alvarombeiadanielmiguel@gmail.com>
2024-04-08 09:24:41 +0100

    working on 0.2.0 releases

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/AlvaroMbeiaDanie1234/flaskfy",
-    version="0.0.1",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=[
         "python-dotenv",
